80823543caf6d8a2ed304d65103187803dfdc753
Aaron Gramlich <gramlich.aaron@gmail.com>
2019-05-02 11:46:41 -0700

    added date fields on prospect model

--- website_app/models.py
+++ website_app/models.py
@@ -11,6 +11,8 @@ class Prospect(models.Model):
 	email = models.EmailField(max_length=256,blank=False)
 	phone = models.CharField(max_length=15,blank=True)
 	message = models.TextField(blank=True)
+	created_date = models.DateTimeField(auto_now_add=True)
+	modified_date = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.first_name + " " + self.last_name + " (" + self.email + ")"
